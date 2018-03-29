3e7f8f445528edeb8cd3c3b21a7342548fbbe1cb
Jenny Zhao <sxz402@gmail.com>
2018-03-29 08:38:29 -0400

    more testing framework stuff

---
+++ jasmine/spec/camra-spec.js
@@ -0,0 +1,19 @@
+
+
+describe("getWeather", function(){
+	it("should return current weather", function(){
+
+});
+});
+
+describe("getSongs", function(){
+	it("should return a list of songs", function(){
+
+});
+});
+
+describe("getLocation", function(){
+	it("should return current Location", function(){
+
+});
+});
