d8fd3b143e03ae61e63a0b7fbb6c3833cda15ab5
Jenny Zhao <sxz402@gmail.com>
2018-04-12 15:37:57 -0400

    poop

--- app.js
+++ app.js
@@ -69,38 +69,37 @@ const server = http.createServer(app);
 server.listen(port, () => console.log(`API running on localhost:${port}`));
 var listSongs;
 app.post('/', function(req, res) {
-   // console.log('gets here');
     var selection = req.body.category;
     var moodoption = req.body.moodoption;
     if (selection == 'weather') {
-      //console.log("enter W");
-	getLocation(function(city) {
-            getWeather(city, function(weather) {
-		getWeatherSongs(weather, function(list) {
-		    attachURLs(list, function(list) {
-          insertDB(list);
-			render(list, res);
-		    });
-		}); 
+	    getLocation(function(city) {
+        getWeather(city, function(weather) {
+          getWeatherSongs(weather, function(list) {
+            attachURLs(list, function(list) {
+              insertDB(list);
+              render(list, res);
             });
-	});
+          }); 
+        });
+      });
     } else if (selection == 'location') {
         getLocation(function(city) {
             getLocationSongs(city, function(list) {
-		          attachURLs(list, function(list) {
+              attachURLs(list, function(list) {
                 insertDB(list);
                 render(list, res);
               });
-        });
             });
+        });
 	
     } else {
-	getMoodSongs(moodoption,function(list) {
-      	    attachURLs(list, function(list) {
-		render(list, res);
-	    });
-	});
-    }
+	      getMoodSongs(moodoption,function(list) {
+      	  attachURLs(list, function(list) {
+            insertDB(list);
+		        render(list, res);
+	        });
+	      });
+      }
 });
 
 hashCode = function(s) {
@@ -111,9 +110,7 @@ hashCode = function(s) {
   return h;
 };
 
-
 function getLocation(callback) {
-
   var city = '';
   request.get('http://ipinfo.io/', function(error, resp, body) {
     if(error){
@@ -121,7 +118,6 @@ function getLocation(callback) {
     }
     var obj = JSON.parse(body);
     city = obj.city;
-   // console.log("inside location" + city);
     callback(city);
   });
 }
@@ -144,16 +140,7 @@ function getLocationSongs(location, callback) {
       list.push(song);
         
     }        
-  /*  Iterator = list.iterate();
-    while ((print = Iterator.next().value) != undefined) {
-      console.log('Song: ' + print.name); 
-      console.log('Artist: ' + print.artist + '\n');
-    }
-    */
-    //res.render(path.join(__dirname, 'views/results.ejs'), {
-    //  songs : list
-    //});
-      callback(list);
+    callback(list);
 
   });
    
@@ -243,73 +230,66 @@ function getMoodSongs(tag, callback) {
 }
 
 function attachURLs(list, callback) {
-  console.log('I entered the url stuff');
-  //console.log("list" + list.song.name); //this is returning undefined
   var input = new List();
   input = list;
   var Nlist = new List();
+  let counter = 0;
   Iterator = input.iterate();
-  
   var spotifyApi = new SpotifyWebApi({
     clientId : '0b4d677f62e140ee8532bed91951ae52',
     clientSecret : 'cc1e617a9c064aa982e8eeaf65626a94'
   });
-    
   // Retrieve an access token.
-  spotifyApi.clientCredentialsGrant().then(function(data) {
+spotifyApi.clientCredentialsGrant().then(function(data) {
 	console.log('The access token expires in ' + data.body['expires_in']);
 	console.log('The access token is ' + data.body['access_token']);
-	token = data.body['access_token'];
-	// Save the access token so that it's used in future calls
-	spotifyApi.setAccessToken(data.body['access_token']);
+  token = data.body['access_token'];
+  // Save the access token so that it's used in future calls
+  spotifyApi.setAccessToken(data.body['access_token']);
 	while ((song = Iterator.next().value) != undefined) {
-	    // console.log("song name: " + song.name);	
-	    // console.log("song artist: " + song.artist);	
-            obj = spotifyApi.searchTracks('track:'+song.name+' artist:'+song.artist)
-		.then(function(data) {  
-		    // console.log(song.url);
-                    
-
-		    //expanded.url = data.body.tracks.items[0].preview_url;
-		    var expanded = new Object();
-
-		    expanded.name = data.body.tracks.items[0].name;
-        expanded.artist = data.body.tracks.items[0].artists[0].name;
-        
-		    if (data.body.tracks.items[0].preview_url != null) {
-			// console.log("inside " + song.name);
-			expanded.url = data.body.tracks.items[0].preview_url;
-			console.log ("HELLO JAVA");
-			console.log('Song name: ' + data.body.tracks.items[0].name + ' Song url: '+ data.body.tracks.items[0].preview_url);
-
-			// console.log(expanded.url);
-			if(expanded.url != null) {
-			    Nlist.push(expanded);
-			}
-		    }
-		    Iterator = Nlist.iterate();
-		    console.log("this is nlist" + Nlist);
-		    while ((print = Iterator.next().value) != undefined) {
-			console.log('Song: ' + print.name); //not on the console, do it on the gui
-			console.log('Artist: ' + print.artist + '\n');
-			console.log("we got here u dumb url");
-      console.log('URL ' + print.url); 
+    counter++;
+    obj = spotifyApi.searchTracks('track:'+song.name+' artist:'+song.artist).then(function(data) {  
+		  var expanded = new Object();
+		  expanded.name = data.body.tracks.items[0].name;
+      expanded.artist = data.body.tracks.items[0].artists[0].name;
+      
+		  if (data.body.tracks.items[0].preview_url != null) {
+        expanded.url = data.body.tracks.items[0].preview_url;
+        console.log('Song name: ' + data.body.tracks.items[0].name + ' Song url: '+ data.body.tracks.items[0].preview_url);
+        if(expanded.url != null) {
+          Nlist.push(expanded);
         }
-        callback(Nlist);
-		}, function(err) {
-		    console.error(err);
-		}).catch(function() { /*console.log("promise rejected")*/});
-	}
-    }, function(err) {
-	    console.log('Something went wrong when retrieving an access token', err);
-    }).catch(function () {
-	    console.log("Promise Rejected");
+      }
+      console.log("NO URL SAD");
+		},function(err) {
+      console.log("why error here");
+      console.error(err);
+    }).catch(function() { console.log("promise rejected")});
+  }
+  console.log ("HELLO JAVA");
+  //Iterator = Nlist.iterate();
+  console.log("this is nlist" + Nlist);
+  console.log(counter);
+  if (counter == 49){
+    callback(Nlist);
+  }
+  // while ((print = Iterator.next().value) != undefined) {
+  //   console.log('Artist: ' + print.artist + '\n');
+  //   console.log('Song: ' + print.name); //not on the console, do it on the gui
+  //   console.log("we got here u dumb url");
+  //   console.log('URL ' + print.url); 
+  // }
+  }, function(err) {
+	  console.log('Something went wrong when retrieving an access token', err);
+  }).catch(function () {
+	  console.log("Promise Rejected");
   });
 }
 
 function insertDB(list){
-  console.log("INSERT INTO THE DB PLEASE");
+  console.log("HELLO " + list.pop());
   Iterator = list.iterate();
+  console.log("u bitch");
   while ((song = Iterator.next().value) != undefined) {
     var a_id = song.name + song.artist;
     var hash = Math.abs(hashCode(a_id));
@@ -318,6 +298,7 @@ function insertDB(list){
     first_instance.save();
   }
 }
+
 function render(list, res) {
   console.log("list to render: " + list);
   res.render(path.join(__dirname, 'views/results.ejs'), {
